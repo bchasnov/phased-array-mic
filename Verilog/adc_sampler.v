8b4a9cda09dbb0505635bd8337becc7dec83d15d
Apoorva Sharma <appi@Apoorvas-MacBook-Pro.local>
2014-11-20 20:27:44 -0800

    adc sampler should work perfectly
    jk, haven’t tested it at all, let alone compile it.

--- Verilog/adc_sampler.v
+++ Verilog/adc_sampler.v
@@ -1,14 +1,15 @@
 module #(parameter LOGCLKDIV) adc_sampler (
 	input logic clk,    // Clock from FPGA
-	output logic[2:0] chnl // Channel selector to ADC
-	output logic n_convst // (ON low) Start conversion on ADC
-	input logic n_eoc // (ON low) Input signal indicating EOC from ADC
+	output logic[2:0] chnl, // Channel selector to ADC
+	output logic n_convst, // (ON low) Start conversion on ADC
+	input logic n_eoc, // (ON low) Input signal indicating EOC from ADC
 	output logic n_cs, // (ON low) chip select to ADC
 	output logic n_rd, // (ON low) initiate a read on ADC
-	output logic[7:0] ch0 // channel zero data
-	output logic[7:0] ch1 // channel one data
-	output logic[7:0] ch2 // channel two data
-	output logic[7:0] ch3 // channel three data
+	input logic[7:0] adc_in, // data bits from the ADC
+	output logic[7:0] ch0, // channel zero data
+	output logic[7:0] ch1, // channel one data
+	output logic[7:0] ch2, // channel two data
+	output logic[7:0] ch3, // channel three data
 	output logic newSample // clock running at overall sampling rate
 	);
 
@@ -38,7 +39,7 @@ always_ff @(posedge clk) begin
 	clockDiv <= clockDiv + 1;
 end
 
-assign stateClk = clockDiv[5]; // fastest clock, for switching states
+assign stateClk = clockDiv[2]; // fastest clock, for switching states
 assign sampleClk = clockDiv[LOGCLKDIV-3]; // clock for reseting the FSM
 assign newSample = clockDiv[LOGCLKDIV-1]; // clock for indicating new samples are ready
 
@@ -82,21 +83,59 @@ end
 
 
 // --------
-// STATE DEPENDENT I/O
+// ADC CONTROLS
 // --------
+
 always_comb begin
 	case (state)
 		START_CONV: 
-
-		WAIT_FOR_EOC:
+			{n_convst, n_cs, n_rd} = 3'b011;
+		
+		WAIT_FOR_EOC: 
+			{n_convst, n_cs, n_rd} = 3'b111;
 
 		CHIP_SELECT: 
+			{n_convst, n_cs, n_rd} = 3'b101;
 
 		READ_DATA:
+			{n_convst, n_cs, n_rd} = 3'b100;
 
 		WAIT_TO_RESET: 
-			
-		default: // shouldn't happen
+			{n_convst, n_cs, n_rd} = 3'b111;
 
+		default: // shouldn't happen
+			{n_convst, n_cs, n_rd} = 3'b111;
 	endcase
 end;
+
+
+// --------
+// CHANNEL SWITCHING
+// --------
+
+always_ff @(posedge stateClk)
+	if (state == CHIP_SELECT) 
+		// load the next address at the chip_select stage
+		curr_channel <= curr_channel + 1;
+
+assign chnl = curr_channel;
+
+
+// --------
+// READING DATA
+// --------
+
+// read data slightly after telling ADC to provide data
+always_ff @(negedge stateClk)
+	if (state == READ_DATA)
+		case (curr_channel)
+			2'd0: ch0 = adc_in;
+			2'd1: ch1 = adc_in;
+			2'd2: ch2 = adc_in;
+			2'd3: ch3 = adc_in;
+			default: ch0 = adc_in;
+		endcase;
+
+
+endmodule;
+
