858d6c07a5d9ec8909bb536b196bba7d77cbb0e3
Benjamin Chasnov <bchasnov@hmc.edu>
2014-11-21 16:47:33 -0800

    made some progress on fft

--- testfft/FFT_controller.sv
+++ testfft/FFT_controller.sv
@@ -1,42 +1,66 @@
-module FFT_controller(
+
+// module FFT_controller(
+module testfft(
 	input                         clk,
 	input                         rst,             // reset on falling edge
 	input                         start,           // start loading 
 	output       [8:0]  inputSampleAddr,  // 0-511
 	input signed [7:0]  inputSampleData,  // 0-255
-	output                        done,	
+	output                        done	
+	
+	
+	
+	
+	
+	
 );
 	
 // parameter ADDR_BITS = 9; // 0-511
 // parameter DATA_BITS = 8; // 0-255
 
-reg [7:0] sink_real;
-reg [3:0] fft_state;
+logic [7:0] sink_real;
+logic [3:0] fft_state;
 
-reg sink_eop;
-reg sink_sop;
+// inputs to the FFT module
+logic sink_sop; // indicates the start of the incoming FFT Frame
+logic sink_eop; // indicates the end of the incoming FFT Frame
+logic sink_valid; // asserted when data on the data bus is valid
+logic sink_error; // 00: no error
+
+// Outputs from the FFT module
+logic sink_ready; // asserted by the FFT engine when it can accept data
+logic source_sop;
+logic source_eop;
+logic source_real;
+logic source_imag;
+
 
 assign sink_imag = 8'b0;   // no input imaginary
-assign sink_error = 2'b00; // no input imaginary
+assign sink_error = 2'b00; // idkwhatimdoing
 
 
 // fft conversion states
 typedef enum logic[3:0] {
-	S_STBY, // standby
-	S_SOP,  // start of packet
-	S_MID,  // in the middle of packet
-	S_EOP   // end of packet
-};
+	IN_STBY, // standby
+	IN_SOP,  // start of packet
+	IN_MID,  // in the middle of packet
+	IN_EOP   // end of packet
+} in_state_t;
 
+typedef enum logic[3:0] {
+	OUT_STBY, // standby
+	OUT_SOP,  // start of packet
+	OUT_MID,  // in the middle of packet
+	OUT_EOP   // end of packet
+} out_state_t;
 
 always @ (posedge start)
 begin
 	// do something?
 end 
 
-
-
-// FFT state machine.
+// FFT state machine
+// Transfers the input data to the FFT machine
 always @ (posedge clk)
 begin
 	if(~rst) // reset at low.
@@ -50,14 +74,17 @@ begin
 	else
 	begin
 		case(fft_state)
-			S_STBY:
+			IN_STBY:
+				/// what do i do here?
 			
-			
-			
-			S_SOP:
+			IN_SOP:
 			begin
+				// TODO: should probably wait for sink_ready to be asserted...
+				
 				// asset start of packet to the fft module
 				sink_sop <= 1'b1;
+				// asseted when data on data bus is valid.
+				sink_valid <= 1'b1;
 				// load the first data point
 				sink_real <= inputSampleData;
 				// load the next address
@@ -65,7 +92,7 @@ begin
 				fft_state <= S_MID;
 			end
 			
-			S_MID:
+			IN_MID:
 			begin
 				// not sop or eop.
 				sink_sop <= 1'b0;
@@ -82,7 +109,7 @@ begin
 					fft_state <= S_MID;
 			end
 			
-			S_EOP:
+			IN_EOP:
 			begin
 				sink_eop <= 1'b1;
 				sink_real <= inputSampleData;
@@ -92,23 +119,38 @@ begin
 	end
 end
 
+// output stuff
+always @ (posedge clk)
+begin
+	case ()
+	if(source_ready)
 
+end
+
+ram512x8 myfftram(
+	.address(),
+	.clock(clk),
+	.data(),
+	.wren(),
+	.q()
+);
 
 fft myfft(
 	.clk(clk),
 	.reset_n(rst),
 	.inverse(1'b0), // 0: FFT, 1: IFFT
-	.sink_valid(),
+	.sink_valid(sink_valid),
 	.sink_sop(sink_sop),
 	.sink_eop(sink_eop),
 	.sink_real(sink_real),
 	.sink_imag(sink_imag),
-	.sink_error(),
+	.sink_error(sink_error),
 	.source_ready(),
-	.sink_ready(),
+	
+	.sink_ready(sink_ready),
 	.source_error(),
-	.source_sop(),
-	.source_eop(),
+	.source_sop(source_sop),
+	.source_eop(source_eop),
 	.source_valid(),
 	.source_exp(),
 	.source_real(),
