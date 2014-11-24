b0c98cbacafb5d1ee717cecdc8f9ae2da5f0f171
Apoorva Sharma <appi2012@gmail.com>
2014-11-23 22:48:59 -0800

    added ram

--- Verilog/adc_test.sv
+++ Verilog/adc_test.sv
@@ -8,10 +8,41 @@ module adc_test (input logic clk,    // Clock from FPGA
 	output logic[7:0] leds,
 	output logic newSample);
 	
-logic[7:0] ch0, ch1, ch2, ch3;
+logic[7:0] ch0, ch1, ch2, ch3, q;
+logic[11:0] addr = 12'b0;
+//logic wren = 1'b1;
+//logic written = 1'b0;
 
 adc_sampler sampler(clk, chnl, n_convst, n_eoc, n_cs, n_rd, adc_in, ch0, ch1, ch2, ch3, newSample);
 
 assign leds = ch0;
+
+
+always_ff @(posedge newSample)
+	addr <= addr + 12'b1;
+	
+/*always_ff @(posedge clk)
+	if (~newSample) 
+		if (~written) begin
+			wren <= 1'b1;
+			written <= 1'b1;
+		end
+		else
+			wren <= 1'b0;
+	else 
+		written <= 1'b0;
+		*/
+
+	
+
+ram4096x8 myram(
+	.address(addr),
+	.clock(clk),
+	.data(ch0),
+	.wren(newSample),
+	.q(q) 
+);
+
+
 
 endmodule
