74c7985b2a1646962b7b46a3aaca3141e82358ab
Apoorva Sharma <appi2012@gmail.com>
2014-12-06 19:17:32 -0800

    Update top.sv

--- Verilog/top.sv
+++ Verilog/top.sv
@@ -7,22 +7,22 @@ module top (
 	output logic n_rd, // (ON low) initiate a read on ADC
 	input logic[7:0] adc_in, // data bits from the ADC
 	output logic[7:0] leds,
+	output logic goodToGo, goodToGo0, goodToGo1,goodToGo2,
+	output logic xcorrDone, samplerDone,
 	input logic start
 	);
 	
 logic[7:0] ch0, ch1, ch2, ch3, q;
 logic[8:0] w_addr = 9'b0;
 logic wren;
-assign leds[7:4] = ch0[7:4];
 
-logic goodToGo0, goodToGo1;
-logic goodToGo;
-logic doneWriting;
-logic doneReading;
+//logic goodToGo0, goodToGo1, goodToGo2;
+//logic goodToGo;
+//logic xcorrDone;
+//logic samplerDone;
 
-assign leds[0] = goodToGo;
-
-assign goodToGo = goodToGo0 & goodToGo1;
+assign goodToGo2 = 1'b1;
+assign goodToGo = goodToGo0 & goodToGo1 & goodToGo2;
 		
 
 adc8x512 sampler(
@@ -35,7 +35,7 @@ adc8x512 sampler(
 	.adc_in(adc_in), // data bits from the ADC
 	
 	.start(goodToGo | start),
-	.doneWriting(doneWriting),    // held high while no more writes are needed
+	.doneWriting(samplerDone),    // held high while no more writes are needed
 	.w_addr(w_addr), // write address
 	.wren(wren), // write enabled
 	.ch0(ch0), 
@@ -50,6 +50,7 @@ logic[7:0] s_addr;
 logic[7:0] a_data, b_data, s_data;
 logic s_wren;
 
+
 xcorr #(9, 8, 8) xcorr_inst (
 	.clk(clk),
 	.start(goodToGo), 
@@ -60,21 +61,19 @@ xcorr #(9, 8, 8) xcorr_inst (
 	.s_addr(s_addr),
 	.s_data(s_data),
 	.s_wren(s_wren),
-	.valid(doneReading)
+	.valid(xcorrDone)
 	);
 
-//assign b_addr = a_addr;
-//assign b_data = a_data;
 	
 pingpong8x512 pingpong0(
 	.clk(clk),
 	.r_addr(a_addr), // read address
 	.r_q(a_data), // read data
-	.readDone(doneReading), // held high while no more reads are needed
+	.readDone(xcorrDone), // held high while no more reads are needed
 	.w_addr(w_addr), // write address
 	.w_data(ch0), // write data
 	.wren(wren), // write enabled
-   .writeDone(doneWriting),// held high while no more writes are needed
+   .writeDone(samplerDone),// held high while no more writes are needed
 	.goodToGo(goodToGo0) // high when reader and writer are good to go
 );
 
@@ -82,26 +81,34 @@ pingpong8x512 pingpong1(
 	.clk(clk),
 	.r_addr(b_addr), // read address
 	.r_q(b_data), // read data
-	.readDone(doneReading), // held high while no more reads are needed
+	.readDone(xcorrDone), // held high while no more reads are needed
 	.w_addr(w_addr), // write address
 	.w_data(ch1), // write data
 	.wren(wren), // write enabled
-   .writeDone(doneWriting),// held high while no more writes are needed
+   .writeDone(samplerDone),// held high while no more writes are needed
 	.goodToGo(goodToGo1) // high when reader and writer are good to go
 );
 
-assign leds[1] = ^ch2 & ^ch3; // to prevent quartus from being too smart
 
-assign leds[2] = doneWriting;
-assign leds[3] = doneReading; 
-
-ram8x512 buffer_b(
+ram32x256 buffer_b(
 	.address(s_addr),
 	.clock(clk),
 	.data(s_data),
 	.wren(s_wren),
 	.q() 
 );
+
+// assign LEDS
+assign leds[0] = goodToGo;
+
+assign leds[1] = ^ch2 & ^ch3; // to prevent quartus from being too smart
+
+assign leds[2] = samplerDone;
+assign leds[3] = xcorrDone; 
+
+assign leds[7:4] = ch0[7:4];
+
+
 
 endmodule
 
