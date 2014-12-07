9a0662ad2c50d2a9feb991b54368a5f634deaf42
Apoorva Sharma <appi2012@gmail.com>
2014-12-06 23:17:16 -0800

    Update argmax.sv

--- Verilog/argmax.sv
+++ Verilog/argmax.sv
@@ -2,15 +2,17 @@
 // argmax computes the maximum and the index of the maximum element in a 
 // RAM with 2^ADDR_WIDTH entries of width DATA_WIDTH. It assumes the RAM
 // reads on the posedge of clk. 
-module argmax #(parameter DATA_WIDTH = 8, parameter ADDR_WIDTH = 12) ( 
-					input logic clk, // system clock
-					input logic start, // pulse high to start computation
-					output logic[ADDR_WIDTH-1:0] dataAddr, // address to RAM
-					input logic[DATA_WIDTH-1:0] data, // data from RAM
-					output logic[DATA_WIDTH-1:0] max, // max value in RAM
-					output logic[ADDR_WIDTH-1:0] maxIndex, // address of max value in RAM
-					output logic valid // high when max and maxIndex are valid
-					);
+module argmax #(parameter DATA_WIDTH = 32, parameter ADDR_WIDTH = 8) ( 
+		input logic clk, // system clock
+		
+		input logic rst,
+		output logic inStandby,   // held high while no more writes are needed
+		
+		output logic[ADDR_WIDTH-1:0] dataAddr, // address to RAM
+		input  logic signed [DATA_WIDTH-1:0]  data, // data from RAM
+		output logic signed [DATA_WIDTH-1:0] max, // max value in RAM
+		output logic[ADDR_WIDTH-1:0] maxIndex // address of max value in RAM
+);
 	
 // INTENRAL LOGIC
 // ram indices
@@ -18,35 +20,37 @@ logic [ADDR_WIDTH-1:0] lastAddr;
 
 // state info
 typedef enum logic[1:0] {
-	WAIT_TO_START,
+	START,
 	COMPUTE,
-	DONE
+	STANDBY
 	} state_t;
 
-state_t state = WAIT_TO_START;
+state_t state = START;
 state_t next_state;
 
 // STATE LOGIC
 always_ff @(posedge clk) begin
-	state <= next_state;
+	if(rst)
+		state <= START;
+	else
+		state <= next_state;
 end
 
 // state transitions
 always_comb begin
 	case (state)
-		WAIT_TO_START:
-			next_state = (start) ? COMPUTE : WAIT_TO_START;
+		START:
+			next_state = COMPUTE;
 		COMPUTE:
-			next_state = (dataAddr == 0) ? DONE : COMPUTE;
-		DONE:
-			next_state = WAIT_TO_START;
+			next_state = (dataAddr == {ADDR_WIDTH{1'b1}}) ? STANDBY : COMPUTE;
+		STANDBY:
+			next_state = STANDBY;
 	endcase
 end
 
 // COMPUTING LOGIC
 always_ff @(posedge clk) begin
 	if (state == COMPUTE) begin
-		valid <= 1'b0;
 		if (data > max) begin
 			max <= data;
 			maxIndex <= lastAddr;
@@ -54,9 +58,13 @@ always_ff @(posedge clk) begin
 		dataAddr <= dataAddr + 1; // increment address
 		lastAddr <= dataAddr;
 	end
-	if (state == DONE) begin
-		valid <= 1'b1;
+	if (state == START) begin
+		dataAddr <= 0;
+		max <= {1'b1, {(DATA_WIDTH-1){1'b0}}};
+		maxIndex <= 0;
 	end
 end
+
+assign inStandby = (state==STANDBY);
 
 endmodule		
